27109b890da5e5840cc6155c196dff1c59c1a519
Danil Katashev <katashnya@gmail.com>
2022-03-02 04:33:35 +0700

    Добавьте файлы проекта.

---
+++ Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace Web2._1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=CalcService}/{action=Index}"
+                    );
+            });
+        }
+    }
+}
